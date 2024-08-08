351877998126de037caa6ed1daa47d0ecda42efe
raj nandani <rajnandani280403@gmail.com>
2024-08-09 00:00:23 +0530

    my commit reactapp

---
+++ src/Apps.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
+
+const Apps = () =>{
+    return(
+        <> 
+            <Sidebar />
+            <Main />
+
+        </>
+    )
+}
+export default Apps;
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
+import { Context } from "../../context/Context";
+
+const Sidebar = () => {
+
+  const [extended, setExtended] = useState(false);
+  const {onSent, prevPrompt, setRecentPrompt, newChat} = useContext(Context);
+
+  const loadPrompt = async (propmt) =>{
+    setRecentPrompt(propmt)
+    await onSent(propmt)
+  }
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div onClick={()=>newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {Array.isArray(prevPrompt) && prevPrompt.map((items, index)=>{
+              return (
+                <div onClick={()=>loadPrompt(items)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{items.slice(0,18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Setting</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
